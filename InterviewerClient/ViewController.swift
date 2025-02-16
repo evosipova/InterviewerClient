e01edba5951c0e3b3d335c372bfc4049e5dd48bb
osipovaliza <osipovaliza@yandex-team.ru>
2025-02-16 18:51:38 +0300

    Initial Commit

---
+++ InterviewerClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterviewerClient
+//
+//  Created by Elizaveta Osipova on 16.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
