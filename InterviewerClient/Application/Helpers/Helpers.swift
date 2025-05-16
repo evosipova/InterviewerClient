d2e6d683006c22c58902c4d3b0816f9bdf9cd0ee
osipovaliza <osipovaliza@yandex-team.ru>
2025-05-16 13:02:00 +0300

    added helpersn

--- InterviewerClient/Application/Helpers/Helpers.swift
+++ InterviewerClient/Application/Helpers/Helpers.swift
@@ -10,3 +10,16 @@ extension View {
         )
     }
 }
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(
+            with: constraintRect,
+            options: .usesLineFragmentOrigin,
+            attributes: [.font: font],
+            context: nil
+        )
+        return ceil(boundingBox.height) + 25
+    }
+}
